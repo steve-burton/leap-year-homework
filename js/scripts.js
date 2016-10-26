ecb0ea6201259fb0906920fc32b9ac104e7f0bac
Steve-Burton <stevevburton@gmail.com>
2016-10-25 20:56:56 -0700

    initial commit with javascript

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//### business logic ###//
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+  return false;
+  }
+};
+
+
+//### user interface logic ###//
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
